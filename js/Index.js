f24584f12b1f2ca9212063d31aead93b9460cb8e
Mikhail Kuzmin <m.kuzmin@darkleaf.ru>
2024-12-14 21:44:55 +0400

    seek && rseek

--- js/Index.js
+++ js/Index.js
@@ -1,38 +1,98 @@
+// asm.js
+// int: x|0
+function searchFirst(keys, key, cmp) {
+  let low = 0;
+  let high = keys.length; // а норм ли постоянно ее спрашивать?
+  while (low < high) {
+    let mid = (high + low) >>> 1;
+    let d = cmp(keys[mid], key);
+    if (d < 0)
+      low = mid + 1;
+    else
+      high = mid;
+  }
+  return low;
+}
+
+function searchLast(keys, key, cmp) {
+  let low = 0;
+  let high = keys.length;
+  while (low < high) {
+    let mid = (high + low) >>> 1;
+    let d = cmp(keys[mid], key);
+    if (d <= 0)
+      low = mid + 1;
+    else
+      high = mid;
+  }
+  return low - 1;
+}
+
+
+// if a === undefined return -1
+// if b === undefined return 1
+
 export default class Index {
   constructor(
     address,
     loader,
-    /*
-      comparator,
-      // а обратный я сам сделаю
-    */
+    comparator,
 
   ) {
     // todo: private?
     this.address = address
     this.loader = loader
+    this.comparator = comparator
   }
 
 
   // fix name
-  async *seekImpl(addr) {
+  // я тут постоянно длину спрашиваю, это ок?
+  async *seekImpl(addr, from) {
     const node = await this.loader(addr)
 
+    let idx = 0
+    // без нее работает
+    //if (from !== undefined)
+    idx = searchFirst(node.keys, from, this.comparator)
+
     if (!!node.addresses) { // branch
-      for (const addr of node.addresses) {
+      for(; idx < node.addresses.length; idx++) {
+        const addr = node.addresses[idx]
         yield* this.seekImpl(addr)
       }
     } else {
-      for (const key of node.keys) {
-        yield key
+      for(; idx < node.keys.length; idx++) {
+        yield node.keys[idx]
       }
     }
   }
 
-  seek(to) {
-    return this.seekImpl(this.address)
+  async *rseekImpl(addr, from) {
+    const node = await this.loader(addr)
+
+    let idx = node.keys.length - 1
+
+    if (from !== undefined)
+      idx = searchLast(node.keys, from, this.comparator)
+
+    if (!!node.addresses) { // branch
+      for(; idx >= 0; idx--) {
+        const addr = node.addresses[idx]
+        yield* this.rseekImpl(addr)
+      }
+    } else {
+      for(; idx >= 0; idx--) {
+        yield node.keys[idx]
+      }
+    }
   }
 
-  //rseek(to) { }
+  seek(from) {
+    return this.seekImpl(this.address, from)
+  }
 
+  rseek(from) {
+    return this.rseekImpl(this.address, from)
+  }
 }
--- js/Index.test.js
+++ js/Index.test.js
@@ -2,37 +2,83 @@ import { describe, expect, test } from "bun:test";
 
 import Index from "./Index.js"
 
-describe("", () => {
-  test("", async () => {
-    const db = {
-      "1": {
-        // type: branch
-        level: 1,
-        keys: [2, 4, 6],
-        addresses: ["2", "3", "4"],
-      },
-      "2": {
-        level: 0,
-        keys: [1, 2],
-      },
-      "3": {
-        level: 0,
-        keys: [3, 4],
-      },
-      "4": {
-        level: 0,
-        keys: [5, 6],
-      },
-    }
+const root = "1"
+const db = {
+  // 2 4 6
+  "1": {
+    // type: branch
+    level: 1,
+    keys: [2, 4, 6],
+    addresses: ["2", "3", "4"],
+  },
+  "2": {
+    level: 0,
+    keys: [2],
+  },
+  "3": {
+    level: 0,
+    keys: [4],
+  },
+  "4": {
+    level: 0,
+    keys: [6],
+  },
+}
 
-    const loader = async (address) => db[address]
+async function loader(address) {
+  return db[address]
+}
 
-    const root = "1"
+function cmp(a, b) {
+  return a - b
+}
 
-    const set = new Index(root, loader)
+const set = new Index(root, loader, cmp)
 
+describe("seek", () => {
+  test("no args", async () => {
     expect(
       await Array.fromAsync(set.seek())
-    ).toEqual([1, 2, 3, 4, 5, 6]);
+    ).toEqual([2, 4, 6]);
+  });
+
+  test("x >= from", async () => {
+    expect(
+      await Array.fromAsync(set.seek(4))
+    ).toEqual([4, 6]);
+
+    expect(
+      await Array.fromAsync(set.seek(3))
+    ).toEqual([4, 6]);
+  });
+
+  test("not in a set", async () => {
+    expect(
+      await Array.fromAsync(set.seek(7))
+    ).toEqual([]);
+  });
+});
+
+describe("rseek", () => {
+  test("no args", async () => {
+    expect(
+      await Array.fromAsync(set.rseek())
+    ).toEqual([6, 4, 2]);
+  });
+
+  test("x <= from", async () => {
+    expect(
+      await Array.fromAsync(set.rseek(4))
+    ).toEqual([4, 2]);
+
+    expect(
+      await Array.fromAsync(set.rseek(5))
+    ).toEqual([4, 2]);
+  });
+
+  test("not in a set", async () => {
+    expect(
+      await Array.fromAsync(set.rseek(1))
+    ).toEqual([]);
   });
 });
