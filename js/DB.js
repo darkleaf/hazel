f13478e968a7eeb9d52f4fa9e91e1c645b9e2013
Mikhail Kuzmin <m.kuzmin@darkleaf.ru>
2024-12-15 22:52:57 +0400

    eav

--- js/DB.js
+++ js/DB.js
@@ -1,30 +1,39 @@
 const e = 0;
 const a = 1;
 const v = 2;
+// на t не смотрим пока, да и не нужно на нее смотреть тут
 
-// значения пока только строки
+// from всегда идет в y в searchFirst & searchLast
+function eav(x, y) {
+  let res = function(){
+    if (y[e] === undefined) return 1
+    if (x[e] > y[e]) return 1
+    if (x[e] < y[e]) return -1
+    if (y[a] === undefined) return 1
+    if (x[a] > y[a]) return 1
+    if (x[a] < y[a]) return -1
+    if (y[v] === undefined) return 1
+    if (x[v] > y[v]) return 1
+    if (x[v] < y[v]) return -1
+    return 0
+  }()
 
-export function eav(x, y) {
-  if (x[e] > y[e]) return 1
-  if (x[e] < y[e]) return -1
+  //console.log("eav", x, y, res)
+  return res
+}
+
+/*
+function aev(x, y) {
   if (x[a] > y[a]) return 1
   if (x[a] < y[a]) return -1
+  if (x[e] > y[e]) return 1
+  if (x[e] < y[e]) return -1
   if (x[v] > y[v]) return 1
   if (x[v] < y[v]) return -1
   return 0
 }
 
-export function aev(x, y) {
-  if (x[a] > y[a]) return 1
-  if (x[a] < y[a]) return -1
-  if (x[e] > y[e]) return 1
-  if (x[e] < y[e]) return -1
-  if (x[v] > y[v]) return 1
-  if (x[v] < y[v]) return -1
-  return 0
-}
-
-export function ave(x, y) {
+function ave(x, y) {
   if (x[a] > y[a]) return 1
   if (x[a] < y[a]) return -1
   if (x[v] > y[v]) return 1
@@ -33,14 +42,27 @@ export function ave(x, y) {
   if (x[e] < y[e]) return -1
   return 0
 }
+*/
 
 import Index from "./Index.js"
 
+class EAVIndex extends Index {
+  async *datoms(fromE, fromA, fromV) {
+    for await (let datom of this.seek([fromE, fromA, fromV])) {
+      if (fromE !== undefined && fromE !== datom[e]) break
+      if (fromA !== undefined && fromA !== datom[a]) break
+      if (fromV !== undefined && fromV !== datom[v]) break
+      yield datom
+    }
+  }
+}
+
+
 export default class DB {
   constructor(roots, loader) {
-    this.eav = new Index(roots.eav, loader, eav)
-    this.aev = new Index(roots.aev, loader, aev)
-    this.ave = new Index(roots.ave, loader, ave)
+    this.eav = new EAVIndex(roots.eav, loader, eav)
+    // this.aev = new Index(roots.aev, loader, aev)
+    // this.ave = new Index(roots.ave, loader, ave)
   }
 
   // читает пока префикс сохраняется
--- js/DB.test.js
+++ js/DB.test.js
@@ -1,1 +1,133 @@
-// тут нужно в кложе генерить данные, я не хочу руками это писать
+import { describe, expect, test } from "bun:test";
+import DB from "./DB.js"
+
+/*
+  (for [i (range 4)
+        j (range 4)]
+    {:i i
+     :j j})
+*/
+
+import ij from "./ij.json"
+const t = ij[0]["max-tx"] // они все в одной транзакции, не отфльтровал я t в clj
+
+async function loader(addr) {
+  return ij[addr]
+}
+
+const db = new DB(
+  {
+    eav: ij[0].eavt,
+  },
+  loader,
+)
+
+describe("eav.datoms", () => {
+  test("", async () => {
+    expect(
+      await Array.fromAsync(db.eav.datoms())
+    ).toEqual([
+      [1,  "i", 0, t],
+      [1,  "j", 0, t],
+      [2,  "i", 0, t],
+      [2,  "j", 1, t],
+      [3,  "i", 0, t],
+      [3,  "j", 2, t],
+      [4,  "i", 0, t],
+      [4,  "j", 3, t],
+      [5,  "i", 1, t],
+      [5,  "j", 0, t],
+      [6,  "i", 1, t],
+      [6,  "j", 1, t],
+      [7,  "i", 1, t],
+      [7,  "j", 2, t],
+      [8,  "i", 1, t],
+      [8,  "j", 3, t],
+      [9,  "i", 2, t],
+      [9,  "j", 0, t],
+      [10, "i", 2, t],
+      [10, "j", 1, t],
+      [11, "i", 2, t],
+      [11, "j", 2, t],
+      [12, "i", 2, t],
+      [12, "j", 3, t],
+      [13, "i", 3, t],
+      [13, "j", 0, t],
+      [14, "i", 3, t],
+      [14, "j", 1, t],
+      [15, "i", 3, t],
+      [15, "j", 2, t],
+      [16, "i", 3, t],
+      [16, "j", 3, t],
+    ]);
+  });
+
+  test("e", async () => {
+    expect(
+      await Array.fromAsync(db.eav.datoms(0))
+    ).toEqual([
+    ]);
+
+    expect(
+      await Array.fromAsync(db.eav.datoms(1))
+    ).toEqual([
+      [1,  "i", 0, t],
+      [1,  "j", 0, t],
+    ]);
+
+    expect(
+      await Array.fromAsync(db.eav.datoms(2))
+    ).toEqual([
+      [2,  "i", 0, t],
+      [2,  "j", 1, t],
+    ]);
+
+    expect(
+      await Array.fromAsync(db.eav.datoms(16))
+    ).toEqual([
+      [16, "i", 3, t],
+      [16, "j", 3, t],
+    ]);
+
+    expect(
+      await Array.fromAsync(db.eav.datoms(17))
+    ).toEqual([
+    ]);
+  });
+
+  test("ea", async () => {
+    expect(
+      await Array.fromAsync(db.eav.datoms(0, "a"))
+    ).toEqual([
+    ]);
+
+    expect(
+      await Array.fromAsync(db.eav.datoms(1, "i"))
+    ).toEqual([
+      [1,  "i", 0, t],
+    ]);
+
+    expect(
+      await Array.fromAsync(db.eav.datoms(17, "z"))
+    ).toEqual([
+    ]);
+  });
+
+  test("eav", async () => {
+    expect(
+      await Array.fromAsync(db.eav.datoms(0, "a", 0))
+    ).toEqual([
+    ]);
+
+    expect(
+      await Array.fromAsync(db.eav.datoms(1, "i", 0))
+    ).toEqual([
+      [1,  "i", 0, t],
+    ]);
+
+    expect(
+      await Array.fromAsync(db.eav.datoms(17, "z", 9))
+    ).toEqual([
+    ]);
+  });
+});
