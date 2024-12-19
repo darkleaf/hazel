2dbd06cd4666155720fa449ddb2ec04b343aafd4
Mikhail Kuzmin <m.kuzmin@darkleaf.ru>
2024-12-20 00:39:03 +0400

    remove immutable & Main

--- js/entrypoint.jsx
+++ js/entrypoint.jsx
@@ -1,7 +1,6 @@
 import { createRoot } from 'react-dom/client';
 import { StrictMode } from 'react';
 
-// import Main from './Main'
 import App from './components/App'
 
 const root = createRoot(document.getElementById('app'))
