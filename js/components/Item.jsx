024b0c11572265776f950f2424336bf13e5e76bf
Mikhail Kuzmin <m.kuzmin@darkleaf.ru>
2024-12-19 23:11:12 +0400

    edit

--- js/components/Item.jsx
+++ js/components/Item.jsx
@@ -1,8 +1,10 @@
+import { useState } from 'react';
 import classnames from "classnames";
+
+import Input from './Input';
 
 export default function Item({ todo, transact }) {
   const {title, completed} = todo;
-  const isWritable = false;
 
   const toggleItem = () => {
     transact([{
@@ -10,18 +12,44 @@ export default function Item({ todo, transact }) {
       completed: !todo.completed,
     }])
   };
-  const handleDoubleClick = () => {};
+
   const removeItem = () => {
     transact([
       ["db.fn/retractEntity", todo.id],
     ])
   };
 
+  const [isWritable, setIsWritable] = useState(false);
+
+  const handleDoubleClick = () => {
+    setIsWritable(true);
+  };
+  const handleBlur = () => {
+     setIsWritable(false);
+  };
+
+  const handleUpdate = (title) => {
+    if (title.length === 0) {
+      removeItem();
+    } else {
+      transact([
+        {
+          "db/id": todo.id,
+          title,
+        }
+      ]);
+    }
+    setIsWritable(false);
+  };
+
   return (
     <li className={classnames({ completed })} data-testid="todo-item">
       <div className="view">
         {isWritable ? (
-          <Input onSubmit={handleUpdate} label="Edit Todo Input" defaultValue={title} onBlur={handleBlur} />
+          <Input onSubmit={handleUpdate}
+                 label="Edit Todo Input"
+                 defaultValue={title}
+                 onBlur={handleBlur} />
         ) : (
           <>
             <input className="toggle" type="checkbox" data-testid="todo-item-toggle" checked={completed} onChange={toggleItem} />
