eba06d2ce15d1ba82b7cd03ed73212206afa1033
Mikhail Kuzmin <m.kuzmin@darkleaf.ru>
2024-12-21 19:33:23 +0400

    utils

--- js/db/Tree.js
+++ js/db/Tree.js
@@ -1,37 +1,4 @@
-// asm.js
-// int: x|0
-function searchFirst(keys, key, cmp) {
-  let low = 0;
-  let high = keys.length; // а норм ли постоянно ее спрашивать?
-  while (low < high) {
-    let mid = (high + low) >>> 1;
-    let d = cmp(keys[mid], key);
-    if (d < 0)
-      low = mid + 1;
-    else
-      high = mid;
-  }
-  return low;
-}
-
-function searchLast(keys, key, cmp) {
-  let low = 0;
-  let high = keys.length;
-  while (low < high) {
-    let mid = (high + low) >>> 1;
-    let d = cmp(keys[mid], key);
-    if (d <= 0)
-      low = mid + 1;
-    else
-      high = mid;
-  }
-  return low - 1;
-}
-
-
-// if a === undefined return -1
-// if b === undefined return 1
-
+import {searchFirst, searchLast} from './utils';
 
 export default class Tree {
   #loader;
---
+++ js/db/utils.js
@@ -0,0 +1,29 @@
+// asm.js
+// int: x|0
+export function searchFirst(keys, key, cmp) {
+  let low = 0;
+  let high = keys.length; // а норм ли постоянно ее спрашивать?
+  while (low < high) {
+    let mid = (high + low) >>> 1;
+    let d = cmp(keys[mid], key);
+    if (d < 0)
+      low = mid + 1;
+    else
+      high = mid;
+  }
+  return low;
+}
+
+export function searchLast(keys, key, cmp) {
+  let low = 0;
+  let high = keys.length;
+  while (low < high) {
+    let mid = (high + low) >>> 1;
+    let d = cmp(keys[mid], key);
+    if (d <= 0)
+      low = mid + 1;
+    else
+      high = mid;
+  }
+  return low - 1;
+}
